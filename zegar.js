7be8a50ca03e18f4c9f27d48c79f092784ba27cb
Karol <karojecz@gmail.com>
2021-06-04 18:36:02 +0200

    ukonczenie menu glowne

--- zegar.js
+++ zegar.js
@@ -15,10 +15,8 @@ function odliczanie()
 		var sekunda = dzisiaj.getSeconds();
 		if (sekunda<10) sekunda = "0"+sekunda;
 		
-		document.getElementById("data").innerHTML = 
-		 dzien+"/"+miesiac+"/"+rok;
-		 document.getElementById("zegar").innerHTML = 
-		 godzina+":"+minuta+":"+sekunda;
+		document.getElementById("zegar").innerHTML = 
+		 dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
 		 
 		 setTimeout("odliczanie()",1000);
 	}
